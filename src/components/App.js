72ec524eb6910ce131ea45c0f56a34d292565687
Cristian Muscalu <cristian.muscalu@mindgeek.com>
2019-01-09 18:39:20 +0200

    initial setup with parcel

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    <h1>w00t</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => (
+    <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    <img width="120" src="" alt="Parcel img"/>
+                </a>
+            </div>
+        </nav>
+    </header>
+)
+
+export default Header
